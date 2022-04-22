fbd99f0f02b6df99f8b681c10ba07b212d5c338b
toligrimm <72781269+toligrimm@users.noreply.github.com>
2022-04-22 12:25:24 +0600

    tshirts added

--- lib/pages/auth/complete_page/complete_page.dart
+++ lib/pages/auth/complete_page/complete_page.dart
@@ -1,4 +1,8 @@
+import 'package:ecom/form_error.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
 
 class CompletePage extends StatefulWidget {
   static String routeName = "/complete_page";
@@ -9,8 +13,88 @@ class CompletePage extends StatefulWidget {
 }
 
 class _CompletePageState extends State<CompletePage> {
+  final List<String?> errors = [];
+  void addError({String? error}) {
+    if (!errors.contains(error)) {
+      setState(() {
+        errors.add(error);
+      });
+    }
+  }
+
+  void removeError({String? error}) {
+    if (errors.contains(error)) {
+      setState(() {
+        errors.remove(error);
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Регистрация'),
+      ),
+      body: Form(
+        child: Column(
+          children: [
+            const Text('Регистрация учетки',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 28,
+                )),
+            const Text('Заполни форму', textAlign: TextAlign.center,
+              style: TextStyle(
+                color: CupertinoColors.secondaryLabel,
+                fontSize: 18,
+              ),),
+            FormError(errors: errors),
+            TextFormField(
+
+              onChanged: (value) {
+                if (value.isNotEmpty) {
+                  removeError(error: kNamelNullError);
+                }
+              },
+              validator: (value) {
+                if (value!.isEmpty && !errors.contains(kNamelNullError)) {
+                  setState(() {
+                    errors.add(kNamelNullError);
+                  });
+                  if (!emailValidatorRegExp.hasMatch(value) &&
+                      !errors.contains(kNamelNullError)) {
+                    setState(() {
+                      errors.add(kNamelNullError);
+                    });
+                  }
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                  labelText: 'Почта',
+                  hintText: 'Введи почту',
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: CupertinoColors.systemOrange,
+                    ),
+                    gapPadding: 10,
+                  ),
+                  suffixIcon: const Icon(CupertinoIcons.mail),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: CupertinoColors.systemOrange,
+                    ),
+                    gapPadding: 10,
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
